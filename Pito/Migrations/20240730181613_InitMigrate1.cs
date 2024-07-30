cc2535bfb5c8cc95d6c9ebb47709df57019afc34
TheJoCrib <92815628+TheJoCrib@users.noreply.github.com>
2024-07-30 20:34:32 +0200

    Post-Production- DEMO
    s

---
+++ Pito/Migrations/20240730181613_InitMigrate1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pito.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMigrate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
